6d0b8decab23fd27e685541efc4f19cea251e672
Elmo <elmotabuzojr.ust@gmail.com>
2023-02-15 18:46:29 +0800

    added re-usable BUTTON component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,3 +1,4 @@
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
@@ -16,7 +17,7 @@ const AddUser = () => {
                 <label htmlFor='age'>Age (Years)</label>
                 <input id='age' type='number'/>
 
-                <button type='submit'>Add User</button>
+                <Button type='submit'>Add User</Button>
             </form>
         </Card>
      );
