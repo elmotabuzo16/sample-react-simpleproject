e2a70b0c4c82ee3b3a261d253c21c791a8957f7b
Elmo <elmotabuzojr.ust@gmail.com>
2023-02-15 18:56:00 +0800

    addign validation and resetting logic

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -11,7 +11,19 @@ const AddUser = () => {
 
     const adddUserHandler = (event) => {
         event.preventDefault();
-        console.log(enteredUsername, enteredAge)
+        
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            return;
+        }
+
+        if (+enteredAge < 1) {
+            return;
+        }
+
+        console.log(enteredUsername, enteredAge);
+
+        setEnteredUsername('');
+        setEnteredAge('');
     };
 
     const usernameChangeHandler = (event) => {
@@ -26,9 +38,9 @@ const AddUser = () => {
         <Card className={classes.input}>
             <form onSubmit={adddUserHandler}>
                 <label htmlFor='username'>Username</label>
-                <input id='username' type='text' onChange={usernameChangeHandler}/>
+                <input id='username' type='text' value={enteredUsername} onChange={usernameChangeHandler}/>
                 <label htmlFor='age'>Age (Years)</label>
-                <input id='age' type='number' onChange={ageChangeHandler}/>
+                <input id='age' type='number' value={enteredAge} onChange={ageChangeHandler}/>
 
                 <Button type='submit'>Add User</Button>
             </form>
